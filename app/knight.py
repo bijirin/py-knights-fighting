dd6492cfdd6dfa5af5de2aba39636030237a39fb
bijirin <tsutrisn@gmail.com>
2026-04-05 21:36:08 +0700

    feat: implement Knight class and associated equipment classes

---
+++ app/knight.py
@@ -0,0 +1,30 @@
+from equipment import Armour, Weapon, Potion
+
+
+class Knight:
+    def __init__(self, name: str, power: int = 0, hp: int = 0) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.protection = 0
+        self.armour = []
+        self.weapon = None
+        self.potion = None
+
+    def equip_armour(self, armour: Armour) -> None:
+        self.armour.append(armour)
+        self.protection += armour.protection
+
+    def equip_weapon(self, weapon: Weapon) -> None:
+        self.weapon = weapon
+        self.power += weapon.power
+
+    def drink_potion(self, potion: Potion) -> None:
+        self.potion = potion
+        for stat, value in potion.effect.items():
+            if stat == "hp":
+                self.hp += value
+            elif stat == "power":
+                self.power += value
+            elif stat == "protection":
+                self.protection += value
